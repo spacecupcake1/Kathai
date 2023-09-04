a46a92b647d8cb78486bb6c0c266047a7963b5a5
Rigani Jegatheeswaran <rigani.jegatheeswaran@roche.com>
2023-09-04 22:36:43 +0200

    new commit

--- src/main/java/com/project/kathai/BookController.java
+++ src/main/java/com/project/kathai/BookController.java
@@ -46,22 +46,6 @@ public class BookController {
             // Get the book object
             Book book = optionalBook.get();
 
-            /* // Construct the URL for the Google Books API
-            String title = book.getTitle();
-            String author = book.getAuthor();
-            String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + title + "+inauthor:" + author;
-
-            // Fetch book details, including the cover image URL, from the Google Books API
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
-
-            if (responseEntity.getStatusCode().is2xxSuccessful()) {
-                String responseBody = responseEntity.getBody();
-                // Parse the JSON response to extract the cover image URL
-                String imageUrl = parseCoverImageUrlFromJson(responseBody);
-                book.setImageUrl(imageUrl);
-            } */
-
             // Add the book to the model
             model.addAttribute("bookDesc", book);
 
@@ -71,9 +55,4 @@ public class BookController {
             return "bookNotFound"; // Create a "bookNotFound.html" template for this case
         }
     }
-
-    /* private String parseCoverImageUrlFromJson(String jsonResponse) {
-        return jsonResponse;
-    }
- */
 }
