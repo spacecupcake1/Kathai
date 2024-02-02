0f8803b383718b9dacaca4c4b5561a51a5f8dcbe
Rigani Jegatheeswaran <rigani.jegatheeswaran@roche.com>
2024-02-02 09:00:50 +0100

    created Reading analysis

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
-from bson import ObjectId
+from typing import Dict
 
 app = Flask(__name__)
 
@@ -35,6 +35,40 @@ def book_desc(id):
     else:
         print("Book not found")
         return render_template('book_not_found.html')
+    
+@app.route('/count')
+def ReadAnalysis():
+
+     # Count by Read
+    read_count = collection.count_documents({'Read': 1})
+    not_read_count = collection.count_documents({'Read': 0})
+
+    # Pages Read and Page Count
+    result = collection.aggregate([
+        {
+            '$group': {
+                '_id': None,
+                'pages_read': {'$sum': '$Pages_Read'},
+                'total_pages': {'$sum': '$Pages'}
+            }
+        }
+    ])
+
+    # Extract the results from the aggregation
+    aggregation_result = next(result, None)
+
+    # Calculate 'pages_unread' as 'total_pages' minus 'pages_read'
+    if aggregation_result:
+     pages_unread = aggregation_result['total_pages'] - aggregation_result['pages_read']
+    
+    # Assuming readCounts and pageCounts are properly defined earlier in your code
+    readCounts = {"Read": read_count, "Not Read": not_read_count}
+    pageCounts = {"read": aggregation_result['pages_read'], "unread": pages_unread}
+
+    print(f"Book found: {result}")
+    return render_template('ReadAnalysis.html', readCounts=readCounts, pageCounts=pageCounts)
+
+
 
 
 if __name__ == '__main__':
